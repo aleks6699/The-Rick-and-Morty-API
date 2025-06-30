35b554db9d69e2b6d4a82068863549658292e28a
Oleksandr <aleks6699@gmail.com>
2025-06-30 19:53:48 +0300

    feat: create example components

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -1,5 +1,66 @@
-function App() {
-  return <div className=" text-amber-300">Hello</div>;
+import { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <MainContent />
+      </div>
+    );
+  }
+}
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <InputSearch />
+        <Button />
+      </header>
+    );
+  }
+}
+class InputSearch extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Search Component</h2>
+      </div>
+    );
+  }
+}
+class Button extends Component {
+  render() {
+    return <button>Click Me</button>;
+  }
+}
+
+class MainContent extends Component {
+  render() {
+    return (
+      <main>
+        <ul>
+          {[1, 2, 3, 4, 5].map((item) => (
+            <CardItem key={item} item={item} />
+          ))}
+        </ul>
+      </main>
+    );
+  }
+}
+interface CardsProps {
+  item: number;
+}
+
+class CardItem extends Component<CardsProps> {
+  render() {
+    return (
+      <li>
+        <h3>Card {this.props.item}</h3>
+        <p>Details about card {this.props.item}</p>
+      </li>
+    );
+  }
 }
 
 export default App;
