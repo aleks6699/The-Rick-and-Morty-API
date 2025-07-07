a647aa0fb3271c5aba543aa2359221a6c049fff2
Oleksandr <aleks6699@gmail.com>
2025-07-07 03:47:02 +0300

    feat: implements link About

--- rs-react-app/src/components/Header/Header.tsx
+++ rs-react-app/src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router';
 
 export function Header({ onClick }: { onClick: (value: string) => void }) {
   const [value, setValue] = useState<string>(
@@ -6,6 +7,24 @@ export function Header({ onClick }: { onClick: (value: string) => void }) {
   );
   return (
     <header className="container flex flex-col mx-auto md:flex-row gap-4 mb-8">
+      <Link
+        to="/about"
+        className="
+    text-white text-lg font-semibold 
+    px-6 py-3 rounded-lg
+    bg-gradient-to-r from-indigo-600 to-purple-600
+    hover:from-indigo-700 hover:to-purple-700
+    transition-all duration-300
+    shadow-lg hover:shadow-xl
+    transform hover:scale-105 active:scale-95
+    border-2 border-transparent hover:border-indigo-400
+    flex items-center justify-center
+    min-w-[120px]
+  "
+      >
+        About
+      </Link>
+
       <input
         type="text"
         className="bg-gray-700 text-white border-0 rounded-lg p-4 text-lg w-full focus:ring-2 focus:ring-blue-400 focus:outline-none placeholder-gray-400 transition-all duration-300 shadow-lg"
